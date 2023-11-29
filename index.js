7288f938cc3f97ba37842b63c9443175d5e1ce71
ItsImpeccable <51460403+itsimpeccable@users.noreply.github.com>
2023-11-29 12:27:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let d = new Date();
 alert(d);
 let poopoo = Math.floor(Math.random() * 1000);
 alert(poopoo);
+var times = 1134324239874238742934732943298;
+for(var i=0; i < times; i++){
+    print("hello");
+}
